80ed2ba1ce24dc02f821d69f6cd1225674515310
unknown <A00833985@tec.mx>
2022-05-10 13:49:46 -0500

    agregar funcionalidad de retangulo

--- paint.py
+++ paint.py
@@ -44,9 +44,19 @@ def circle(start, end):
 
 
 def rectangle(start, end):
-    """Draw rectangle from start to end."""
-    pass  # TODO
+    "Draw rectangle from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
 
+    for count in range(2):
+        forward(end.x - start.x)
+        left(90)
+        forward(end.x + start.x)
+        left(90)
+
+    end_fill()
 
 def triangle(start, end):
     """Draw square from start to end."""
